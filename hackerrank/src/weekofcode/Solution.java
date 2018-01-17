8acd4283a27dffdc5710aca2df72b2530c4b5ed2
shivam <ssukggo@gmail.com>
2018-01-17 23:14:03 +0530

    week of code commit.

---
+++ hackerrank/src/weekofcode/Solution.java
@@ -0,0 +1,12 @@
+package weekofcode;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		String[] names = {"shivam", "varun", "tejas"};
+		for(String name : names)
+			System.out.println(name);
+
+	}
+
+}
