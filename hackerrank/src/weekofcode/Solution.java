7ecec6761ebef2bf0045f78b05ee233fb3467aef
shivam <ssukggo@gmail.com>
2018-01-17 23:24:48 +0530

    dfdsf

--- hackerrank/src/weekofcode/Solution.java
+++ hackerrank/src/weekofcode/Solution.java
@@ -3,10 +3,11 @@ package weekofcode;
 public class Solution {
 
 	public static void main(String[] args) {
-		String[] names = {"shivam", "varun", "tejas"};
-		int i = 0;
-		while(i < names.length)
-			System.out.println(names[i++]);
+		String[] names = {"shivam", "varun", "tejas","om"};
+		for(String name : names){
+			System.out.println(name);
+			System.out.println("hello there: ");
+		}
 
 	}
 
